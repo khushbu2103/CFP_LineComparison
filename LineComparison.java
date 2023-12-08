3976a702c8912845262f4340a591766070c61778
khushbu <khushbusoni475@gmail.com>
2023-12-08 11:30:01 +0530

    [khushbu] adding quality of 2 length

--- LineComparison.java
+++ LineComparison.java
@@ -6,16 +6,36 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to the line comparison program");
         Scanner sc = new Scanner(System.in);
+        System.out.println("please enter co-ordinates of line 1st");
         System.out.println("Enter x1 as number");
-        float x1 = sc.nextFloat();
+        float Ax1 = sc.nextFloat();
         System.out.println("Enter x2 as number");
-        float x2 = sc.nextFloat();
+        float Ax2 = sc.nextFloat();
         System.out.println("Enter y1 as number");
-        float y1 = sc.nextFloat();
+        float Ay1 = sc.nextFloat();
         System.out.println("Enter y2 as number");
-        float y2 = sc.nextFloat();
+        float Ay2 = sc.nextFloat();
 
-        float lineLength = (float) Math.sqrt(Math.pow(x1-x2, 2)+ Math.pow(y1-y2, 2));
-        System.out.println("Length of line : " + lineLength);
+        System.out.println("please enter co-ordinates of line 2nd");
+        System.out.println("Enter x1 as number");
+        float Bx1 = sc.nextFloat();
+        System.out.println("Enter x2 as number");
+        float Bx2 = sc.nextFloat();
+        System.out.println("Enter y1 as number");
+        float By1 = sc.nextFloat();
+        System.out.println("Enter y2 as number");
+        float By2 = sc.nextFloat();
+
+        float lineLength1 = (float) Math.sqrt(Math.pow(Ax1-Ax2, 2)+ Math.pow(Ay1-Ay2, 2));
+        System.out.println("Length of line1 : " + lineLength1);
+
+        float lineLength2 = (float) Math.sqrt(Math.pow(Bx1-Bx2, 2)+ Math.pow(By1-By2, 2));
+        System.out.println("Length of line2 : " + lineLength2);
+
+        if (lineLength1 == lineLength2)
+            System.out.println("Line1 is equals to line2");
+        else
+            System.out.println("Line1 is not equals to line2");
+
     }
 }
